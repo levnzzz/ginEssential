53e58e68f1c7e33120693c1ed4ed7cd0bb00a378
levnzzz <782142320@qq.com>
2021-11-23 19:20:56 +0800

    使用jwt中间件

---
+++ middleware/AuthMiddleware.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/levnzzz/ginEssential/common"
+	"github.com/levnzzz/ginEssential/model"
+	"net/http"
+	"strings"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		//获取authorization header
+		tokenString := ctx.GetHeader("Authorization")
+		//validate token formate
+		if tokenString == "" || !strings.HasPrefix(tokenString, "Bearer") {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
+			ctx.Abort()
+			return
+		}
+
+		tokenString = tokenString[7:]
+		token, claims, err := common.PareseToken(tokenString)
+		if err != nil || !token.Valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
+			ctx.Abort()
+			return
+		}
+		// 验证通过后获取 claim 中的 userid
+		userId := claims.UserId
+		DB := common.GetDB()
+		var user model.User
+		DB.First(&user, userId)
+
+		//用户
+		if user.ID == 0{
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
+			ctx.Abort()
+			return
+		}
+
+		//用户存在，将 user 的信息写入上下文
+		ctx.Set("user", user)
+		ctx.Next()
+
+	}
+}
