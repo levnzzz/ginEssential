e49d848f4aabb77e4b241d80a25aa7ae0c5640cc
levnzzz <782142320@qq.com>
2021-11-23 15:55:09 +0800

    登陆注册功能全部全部完成

--- controller/UserController.go
+++ controller/UserController.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/levnzzz/ginEssential/common"
 	"github.com/levnzzz/ginEssential/model"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
@@ -17,7 +18,7 @@ func Register(ctx *gin.Context) {
 	telephone := ctx.PostForm("telephone")
 	password := ctx.PostForm("password")
 	//数据验证
-	if len(telephone) != 11 {
+	if len(telephone) < 11 {
 		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code" : 422, "msg" : "手机号必须为11位"})
 		return
 	}
@@ -39,17 +40,61 @@ func Register(ctx *gin.Context) {
 	}
 
 	//创建用户
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误"})
+	}
 	newUser := model.User{
 		Name: name,
 		Telephone: telephone,
-		Password: password,
+		Password: string(hasedPassword),
 	}
 
 	DB.Create(&newUser)
 	//返回结果
 	ctx.JSON(200, gin.H{
+		"code": 200,
 		"msg": "注册成功",
 	})
+}
+
+func Login(ctx *gin.Context) {
+	db := common.DB
+	//获取参数
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+	//数据验证
+	if len(telephone) < 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code" : 422, "msg" : "手机号必须为11位"})
+		return
+	}
+
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code" : 422, "msg" : "密码必须6位数"})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code" : 422, "msg" : "用户不存在"})
+		return
+	}
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code" : 400, "msg": "密码错误"})
+		return
+	}
+	//发放token
+	token := "11"
+	//返回结果
+	ctx.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{"token" : token},
+		"msg": "登陆成功",
+	})
+
 }
 
 
